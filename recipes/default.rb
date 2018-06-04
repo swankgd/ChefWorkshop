aa4c76807e7b2a3c2f66b0d84e132b248fb29947
swankgd <swankgd@gmail.com>
2018-06-04 15:13:10 -0700

    v0.2.2 added [mongodb] section header to repo file

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,8 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 file '/etc/yum.repos.d/mongodb.repo' do
-	content 'name=MongoDB Repository
+	content '[mongodb]
+	name=MongoDB Repository
 	baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/
 	gpgcheck=0
 	enabled=1'
